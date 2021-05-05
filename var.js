92c67534afc4e5c66f4beac6adf93ab113ce181c
GooDKnopKa <GooDKnopKa@internet.ru>
2021-05-05 22:31:57 +1000

    Третий час

--- var.js
+++ var.js
@@ -1,12 +1,15 @@
-var div5 = document.getElementById('three3').getElementsByTagName('p'); // Возвращает 1 элеменит
-// // var par = div5.getElementsByTagName('p'); // Возвращает масив
-// // var div = document.getElementsByTagName('div'); // Возвращает масив
+var div5 = document.getElementById('three3'); // Возвращает 1 элеменит
+var div5 = document.getElementById('three3').getElementsByTagName('p'); // Возвращает массив параграфов (p)
+// // var par = div5.getElementsByTagName('p'); // Возвращает масив параграфов(p)
+// // var div = document.getElementsByTagName('div'); // Возвращает масив дивов (div)
 // console.log(div5);
 div5[2].style.background = "#73bcff";
 // ===================================
-var div2 = document.querySelector('div p') // CSS селекторы / Можно работать как в CSS коде / Лишь первый параграф первого элемента
+var div2 = document.querySelector('div p') // CSS селекторы / Можно работать как в CSS коде / Лишь первый параграф первого элемента, то есть, 1 элемент
 var div2 = document.querySelector('#five5') // CSS селекторы / Можно работать как в CSS коде / Вернет элемент по его ID(ID выделяется через #)
-var div2 = document.querySelector('#five5 p') // CSS селекторы / Можно работать как в CSS коде / Вернет стиль в эллменте под ID(ID выделяется через #)
-var div3 = document.querySelectorAll('div2') // CSS селекторы / Можно работать как в CSS коде / Вернет стиль в эллменте под ID(ID выделяется через #)
-div2.style.background = "purple";
-console.log(div2);
+// var div2 = document.querySelector('#five5 p') // CSS селекторы / Можно работать как в CSS коде / Вернет стиль в эллменте под ID(ID выделяется через #)
+var div3 = document.querySelectorAll('div') // CSS селекторы / Можно работать как в CSS коде / Вернет массив дивов(div)
+div3[3, 2, 1, 0].style.background = "purple";
+console.log(div3);
+// ===================================
+three3.style.background = 'orange'
